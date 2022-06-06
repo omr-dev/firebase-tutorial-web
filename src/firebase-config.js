0239b6180fff14f3374c11351ec7ca2622fde793
omer <peynirciomerfaruk@gmail.com>
2022-06-06 16:51:01 +0200

    firebase configuration adde

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,10 +6,28 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
-const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
-};
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+
+
+const config = {
+  apiKey: "AIzaSyAfmTcIXJWLPo6Ao3LFh4OPr8XbYPwRl-c",
+  authDomain: "tutorial-friendlychat-1d3fb.firebaseapp.com",
+  projectId: "tutorial-friendlychat-1d3fb",
+  storageBucket: "tutorial-friendlychat-1d3fb.appspot.com",
+  messagingSenderId: "163459343901",
+  appId: "1:163459343901:web:60f0820296c73b5132c8f4",
+  measurementId: "G-8K8K66J6K6"
+};
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
@@ -18,3 +36,7 @@ export function getFirebaseConfig() {
     return config;
   }
 }
+
+// Initialize Firebase
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
